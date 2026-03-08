d0d69eb91abf973e345db133cc9301fcec2d92a8
Habil <habilmuha14@gmail.com>
2026-03-08 17:12:35 +0700

    feat: setup frontend routing and app structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar.jsx'
+import Home from './pages/Home.jsx'
+import Search from './pages/Search.jsx'
+import Detail from './pages/Detail.jsx'
+import Cart from './pages/Cart.jsx'
+import Orders from './pages/Orders.jsx'
 
-function App() {
+export default function App() {
+  const [cart, setCart] = useState([])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar cart={cart} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/book/:key" element={<Detail cart={cart} setCart={setCart} />} />
+        <Route path="/cart" element={<Cart cart={cart} setCart={setCart} />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+    </>
+  )
 }
-
-export default App;
